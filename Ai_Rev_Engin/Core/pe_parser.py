c03fa7155460d935feed842edb2850d4ec142336
Youness Ben <tudorbenhadda@gmail.com>
2026-05-05 15:34:10 +0100

    Adding Methods

--- Ai_Rev_Engin/Core/pe_parser.py
+++ Ai_Rev_Engin/Core/pe_parser.py
@@ -79,6 +79,69 @@ class PeParser:
             
         return False
     
+    def get_imports(self):
+        if not self.pe:
+            return []
+        
+        imports = []
+
+        if not hasattr(self.pe, "DIRECTORY_ENTRY_IMPORT"): # We check if the file have imports
+            return imports
+        
+        for entry in self.pe.DIRECTORY_ENTRY_IMPORT:
+            dll_name = entry.dll.decode('utf-8', errors = 'ignore')
+
+            for imp in entry.imports:
+                if imp.name:
+                    api_name = imp.name.decode('utf-8', errors = 'ignore')
+
+                else:
+                    api_name = f"Ordinal_{imp.ordinal}"
+
+                    imports.append({
+                        'dll': dll_name,
+                        'api': api_name,
+                        'ordinal': imp.ordinal
+                    })
+
+        return imports
+    
+    def get_dangerous_apis(self):
+
+        dangerous_list = {
+            # Process Injection
+            'CreateRemoteThread', 'WriteProcessMemory', 'VirtualAllocEx',
+            'ReadProcessMemory', 'OpenProcess', 'NtOpenProcess',
+
+            #Command Exec
+            'WinExec', 'ShellExecute', 'system', 'popen',
+
+            #NetWork
+            'URLDownloadToFile', 'InternetOpen', 'InternetConnect',
+            'HttpOpenRequest', 'HttpSendRequest', 'socket', 'connect',
+
+            #Persistence
+            'RegSetValue', 'RegCreateKey', 'RegOpenKey',
+
+            #Anti-Debug
+            'IsDebuggerPresent', 'CheckRemoteDebuggerPresent',
+
+            #Encryption
+            'CryptEncrypt', 'CryptDecrypt', 'CryptAcquireContext',
+
+            #File Ope
+            'CreateFile', 'WriteFile', 'DeleteFile', 'MoveFile'
+        }
+
+        all_imports = self.get_imports()
+
+        dangerous = []
+        for imp in all_imports:
+            if imp['api'] in dangerous_list:
+                dangerous.append(imp)
+
+            return dangerous
+    
 
 if __name__ == '__main__':
     import sys
@@ -114,6 +177,18 @@ if __name__ == '__main__':
         else:
             print("\n File is NOT packed.")
             print("   Static analysis will work normally.")
+
+        imports = parser.get_imports()
+        print(f"\n[IMPORTS]")
+        print(f"  Total APIs imported: {len(imports)}")
+        
+        dangerous = parser.get_dangerous_apis()
+        if dangerous:
+            print(f"\n  DANGEROUS APIs Found ({len(dangerous)}):")
+            for imp in dangerous[:15]:  # Show first 15
+                print(f"    - {imp['api']} (from {imp['dll']})")
+        else:
+            print(f"\n  No dangerous APIs detected")
 
     else:
         print("Not a valid PE file")
