276d4b9a80edbb0dbc3bab732bf5fbc620355c26
Youness Ben <tudorbenhadda@gmail.com>
2026-05-10 14:11:34 +0100

    Working on EDR

--- Ai_Rev_Engin/Core/EDR_Watcher/edr_watcher.py
+++ Ai_Rev_Engin/Core/EDR_Watcher/edr_watcher.py
@@ -37,3 +37,86 @@ class EDRWATCHER:
         print(f"Monitoring: {folder_path}")
         print("Analyzing new .exe/.dll/.scr/.sys files in real-time")
         print("Press Ctrl+C to stop\n")
+
+        handler = MalwarHandler(self.analyze_new_file)
+        Observer = Observer()
+        Observer.schedule(handler, str(folder_path), recursive= recursive)
+
+        try:
+            while True:
+                time.sleep(1)
+        
+        except KeyboardInterrupt:
+            Observer.stop()
+            print("File Watcher stopped")
+
+        Observer.join()
+
+    def analyze_new_file(self, file_path):
+        print(f"\n" + "=" * 60)
+        print(f"NEW FILE DETECTED!")
+        print(f"File: {file_path.name}")
+        print(f"Path: {file_path}")
+        print(f"Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+        print("=" * 60)
+
+        try:
+            with open(file_path, 'rb') as f:
+                if f.read(2) != b'MZ':
+                    print("This is not a PE File !")
+                    return
+                
+            parser = PeParser(str(file_path))
+            if not parser.load():
+                print("Failed to analyze !")
+                return
+            
+            dangerous = parser.get_dangerous_apis()
+            score_data = parser.calculate_score()
+            verdict = parser.get_verdict(score_data['score'])
+
+            print(f"\n" + "=" * 60)
+            print(f"NEW FILE DETECTED!")
+            print(f"File: {file_path.name}")
+            print(f"Path: {file_path}")
+            print(f"Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+            print("=" * 60)
+
+            if dangerous:
+                print(f"\n Dangerous APIs found ({len(dangerous)}) : ")
+
+                for api in dangerous[:5]:
+                    print(f" - {api['api']} ({api['dll']})")
+
+            self.log_event(file_path, score_data['score'], verdict, dangerous)
+
+            # Alert
+
+            if score_data['score'] >= 70:
+                self.send_alert(file_path,score_data['score'], verdict, dangerous)
+                self.quarantine_file(file_path)
+                self.terminate_process(file_path)
+
+            db = db_manager()
+
+            results = {
+                'file_name': file_path.name,
+                'sha256': parser.compute_sha256(),
+                'file_size': file_path.stat().st_size,
+                'entropy': parser.get_entropy(),
+                'is_packed': parser.is_packed,
+                'score': score_data['score'],
+                'verdict': verdict,
+                'dangerous_apis': dangerous,
+                'imports': parser.get_imports()[:50]
+            }
+
+            db.save_analysis(results)
+            db.close()
+
+            print("\n Analysis saved in Data_Base !")
+
+        except Exception as e:
+            print(f"Error analyzing the File : {e}")
+
+    
